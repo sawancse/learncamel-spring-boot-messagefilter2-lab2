4ac5a6a697ed4553b02cd24de5dc7b3282d59a23
sawancse <sawancse@gmail.com>
2021-10-27 08:03:51 +0530

    Add files via upload

---
+++ src/main/java/com/learncamel/bean/MyBean.java
@@ -0,0 +1,26 @@
+package com.learncamel.bean;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Header;
+
+public class MyBean { 
+	public boolean isGoldCustomer(@Header("level") String level)
+	 { 
+		System.out.println("calling isGoldCustomer m/d");
+		return level.equals("gold"); 
+	}
+	
+	public boolean isGoldCustomerHeader(@Header("level") String level, @Header("param1") String param1)
+	 { 
+		System.out.println("calling isGoldCustomer m/d");
+		return level.equals("gold"); 
+	}
+	
+	 public boolean isGoldCustomerExchange(Exchange  exchange) {
+		 System.out.println("calling isGoldCustomerExchange m/d");
+		 if(exchange.getIn().getHeader("level").equals("gold"))
+			 return true;
+		return false;
+	  }
+	 
+ } 
